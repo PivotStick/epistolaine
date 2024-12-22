c73bea6e651b2029fe9d3f3d1595711200b08792
PivotStick <pivoorigami@gmail.com>
2024-12-22 19:44:17 +0100

    fixed home pag

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -4,9 +4,9 @@ import { db } from '$lib/server/db';
  * @type {import("./$types").LayoutServerLoad}
  */
 export async function load() {
-	const items = await db.items.find();
+	const products = await db.products.find();
 
 	return {
-		items
+		products
 	};
 }
